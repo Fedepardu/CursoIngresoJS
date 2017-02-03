55c897b9c071bfd54a47f32f093e11de4e15efb4
Fedepardu <fedepardu@gmail.com>
2017-02-02 22:11:29 -0300

    Segundo Commit

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,18 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre;
+	nombre = prompt("Ingrese su nombre","Federico")
+	alert(nombre);
+
+
+
+
+
+
+
+	var person = prompt("Ingresa tu nombre", "NATALIANATALIA");
+
+}
+
---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	var nombre;
+	var edad;
+	nombre = document.getElementById('elNombre').value;
+	edad = document.getElementById('laEdad').value;
+	alert("Usted se llama " + nombre + " y tiene " + edad + " años");
+
+
+
+
+
+
+	/*Lo que hago en el ultimo de alert, es CONCATENAR es decir juntar las dos variables para que me las muestre juntos.
+	Es meter letras entre variables, o variables con variables.
+
+
+	*/
+
+
+}
+
