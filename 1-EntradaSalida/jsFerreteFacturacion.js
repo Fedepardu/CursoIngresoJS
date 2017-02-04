b2137e3caed899c3780651736ffdd1727c0bef2f
Fedepardu <fedepardu@gmail.com>
2017-02-03 21:39:50 -0300

    Ejercicio FERRETE FACTURACION TERMINADO

---
+++ 1-EntradaSalida/jsFerreteFacturacion.js
@@ -0,0 +1,84 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+function Sumar () 
+{
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var resultado;
+
+
+	precioUno = document.getElementById('PrecioUno').value;
+	precioDos= document.getElementById('PrecioDos').value;
+	precioTres = document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+
+	resultado=precioUno+precioDos+precioTres;
+
+
+	alert("La suma es " + resultado);
+
+
+}
+function Promedio () 
+{
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var resultado;
+	
+
+	precioUno= document.getElementById('PrecioUno').value;
+	precioDos= document.getElementById('PrecioDos').value;
+	precioTres= document.getElementById('PrecioTres').value;
+
+	precioUno= parseInt(precioUno);
+	precioDos= parseInt(precioDos);
+	precioTres= parseInt(precioTres);
+
+	resultado =(precioUno + precioDos + precioTres) / 3
+
+	
+
+
+
+	alert("El promedio es " + resultado);
+
+
+
+
+
+}
+function PrecioFinal () 
+{
+	
+	var precioUno;
+	var PrecioDos;
+	var PrecioTres;
+	var resultado;
+
+	precioUno = document.getElementById('PrecioUno').value;
+	precioDos = document.getElementById('PrecioDos').value;
+	precioTres = document.getElementById('PrecioTres').value;
+
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres = parseInt(precioTres);
+
+	resultado = (precioUno + precioDos + precioTres) * 1.21;
+
+	alert("El precio final es " + resultado);
+
+
+
+
+
+
+}
