4a1a4045fea9a37b137f60591f1501c385ee74cf
Fedepardu <fedepardu@gmail.com>
2017-02-09 22:07:07 -0300

    ferrete construccion TERMINADO

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,85 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo;
+	var ancho;
+	var radio;
+	var resultado;
+
+	
+
+	largo = document.getElementById('Largo').value;
+	ancho = document.getElementById('Ancho').value;
+	radio = document.getElementById('Radio').value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+	radio = parseInt(radio);
+
+	resultado = (largo+ancho) * 3;
+	
+
+	alert("La cantidad es " + resultado);
+
+
+
+
+
+
 
 }
 function Circulo () 
 {
-	
+	var radio;
+	var area;
+	var resultado;
+
+	radio = document.getElementById('Radio').value;
+
+	radio = parseInt(radio);
+	area = parseInt(area);
+
+	area = radio * 2 * 3.14;
+
+	resultado = area * 3;
+
+	alert(resultado);
+
+
+
 }
 function Materiales () 
 {
+	var largo;
+	var ancho;
+	var area;
+	var cemento;
+	var cal;
+	var resultado;
+
+
+	largo = document.getElementById('Largo').value;
+	ancho = document.getElementById('Ancho').value;
+
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	area = largo*ancho
+
+	cemento = area*2
+	cal = area*3
+
+	resultado = cemento + cal;
 	
+
+
+	alert("Bolsas de son " + cemento + " De cal son " + cal);
+
+	
+
+
+
+
+
 }
