46f36a2b7297f614369bbc25a704f63e846c9cc8
Fedepardu <fedepardu@gmail.com>
2017-02-11 11:51:39 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,29 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var notarandom;
 
-}//FIN DE LA FUNCIÓN
+	notarandom = Math.floor((Math.random() * 10) + 1);
+
+	alert(notarandom);
+
+	if (notarandom== 9 || notarandom==10) 
+		{
+			alert("EXCELENTE");
+
+		} else 
+			{
+			if (notarandom > 4) 
+				{
+					alert("Aprobaste");
+
+				} else 
+				{
+					alert("la proxima se puede");
+				}
+		}
+
+	
+
+
+}
